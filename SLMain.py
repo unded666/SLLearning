89811cfac3912a576795344ebe2c9aa028c7a5f5
Evan Hurwitz <evan.hurwitz@avast.com>
2024-04-09 23:30:37 +0100

    added triangle wave for shits and giggles

--- SLMain.py
+++ SLMain.py
@@ -14,15 +14,27 @@ def sawtooth_data(freq=1, amp=1, phase=0, n=100):
     df = pd.DataFrame({'x': x, 'y': y})
     return df
 
+def triangle_data(freq=1, amp=1, phase=0, n=100):
+    x = np.linspace(0, 2*np.pi, n)
+    y = amp * (2 * (x * freq + phase) / (2*np.pi) % 1 - 0.5)
+    y = 2 * np.abs(y) - 1
+    df = pd.DataFrame({'x': x, 'y': y})
+    return df
+
 def plot_data(df: pd.DataFrame):
     st.line_chart(data=df, x='x', y='y')
 
 def hello_world():
     st.write("Hello, world!")
     st.write("Here's some data:")
+    wave_titles = ['Sine wave',
+                   'Sawtooth wave',
+                   'Triangle wave']
     df_sine = sinu_data()
     df_saw = sawtooth_data()
-    for data in [df_sine, df_saw]:
+    df_tri = triangle_data()
+    for data, title in zip([df_sine, df_saw, df_tri], wave_titles):
+        st.write(f"This is a {title} waveform")
         plot_data(data)
 
 def main():
