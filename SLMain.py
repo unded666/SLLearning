b9d52b36ba2588dc1d62e37b7f8b80eca51792b7
Evan Hurwitz <evan.hurwitz@avast.com>
2024-04-09 23:46:03 +0100

    added gaussian data

--- SLMain.py
+++ SLMain.py
@@ -28,6 +28,12 @@ def square_data(freq=1, amp=1, phase=0, n=100):
     df = pd.DataFrame({'x': x, 'y': y})
     return df
 
+def gaussian_data(n=100):
+    x = np.linspace(-5, 5, n)
+    y = np.exp(-x**2)
+    df = pd.DataFrame({'x': x, 'y': y})
+    return df
+
 def plot_data(df: pd.DataFrame):
     st.line_chart(data=df, x='x', y='y')
 
@@ -37,12 +43,14 @@ def hello_world():
     wave_titles = ['Sine wave',
                    'Sawtooth wave',
                    'Triangle wave',
-                   'Square wave']
+                   'Square wave',
+                   'Gaussian wave']
     df_sine = sinu_data()
     df_saw = sawtooth_data()
     df_tri = triangle_data()
     df_sq = square_data()
-    for data, title in zip([df_sine, df_saw, df_tri, df_sq], wave_titles):
+    df_gauss = gaussian_data()
+    for data, title in zip([df_sine, df_saw, df_tri, df_sq, df_gauss], wave_titles):
         st.write(f"This is a {title} waveform")
         plot_data(data)
 
